1283766bc422f4d36a4d9ebf3f1d413192387be4
Gianpaolo Valero <givalero@microsoft.com>
2018-06-29 10:55:28 +0100

    Adding Microsoft XRM SDK library

--- LeadO/Class1.cs
+++ LeadO/Class1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Xrm.Sdk;
 namespace LeadO
 {
-    public class Class1
+    public class pluginclass : IPlugin
     {
+        public void Execute (IServiceProvider serviceProvider)
+        {
+
+        }
     }
 }
