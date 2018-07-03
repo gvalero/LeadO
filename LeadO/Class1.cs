212670c116eabf53048962740cdb7d343d0c8203
Gianpaolo Valero <givalero@microsoft.com>
2018-07-03 15:41:37 +0100

    Add example to code

--- LeadO/Class1.cs
+++ LeadO/Class1.cs
@@ -8,9 +8,39 @@ namespace LeadO
 {
     public class pluginclass : IPlugin
     {
-        public void Execute (IServiceProvider serviceProvider)
+        public void Execute(IServiceProvider serviceProvider)
         {
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
+            // Get a reference to the Organization service.
+            IOrganizationServiceFactory factory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters != null)
+            {
+                //entity = (Entity)context.InputParameters["Target"];
+                //Instead of getting entity from Target, we use the Image
+                Entity entity = context.PostEntityImages["PostImage"];
+
+                Money rate = (Money)entity.Attributes["po_rate"];
+                int units = (int)entity.Attributes["po_units"];
+                EntityReference parent = (EntityReference)entity.Attributes["po_parentid"];
+
+                //Multiply
+                Money total = new Money(rate.Value * units);
+
+                //Set the update entity
+                Entity parententity = new Entity("po_parententity");
+                parententity.Id = parent.Id;
+                parententity.Attributes["po_total"] = total;
+
+                //Update
+                service.Update(parententity);
+            }
         }
+
     }
 }
